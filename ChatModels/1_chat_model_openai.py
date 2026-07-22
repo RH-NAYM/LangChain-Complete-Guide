4f1431915620747ca490db2bc42ce098d1bc43a8
rakib72642 <naym.mj@gmail.com>
2026-07-22 18:17:34 +0600

    Add initial implementation of OpenAI, Anthropic, and Google chat models; update requirements

---
+++ ChatModels/1_chat_model_openai.py
@@ -0,0 +1,14 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+model = ChatOpenAI(
+    model="gpt-3.5-turbo"
+)
+
+result = model.invoke("What is the capital of Bangladesh?")
+
+print(result.content)
+
